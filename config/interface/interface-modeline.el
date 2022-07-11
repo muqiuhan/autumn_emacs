b46af065867cfd364a3c967d6eadbeb1cb3a04d7
Muqiu Han <muqiu-han@outlook.com>
2022-07-11 18:05:20 +0800

    Modify modeline configuration

--- config/interface/interface-modeline.el
+++ config/interface/interface-modeline.el
@@ -41,6 +41,65 @@
          (replace-name (plist-get replace-table (intern major-name))))
     (if replace-name replace-name major-name)))
 
+(defun pretty-default-modeline ()
+  (interactive)
+  (setq-default mode-line-format
+		(list
+		 ;; the buffer name; the file name as a tool tip
+		 " "
+		 '(:eval (propertize "%b " 'face 'font-lock-keyword-face
+				     'help-echo (buffer-file-name)))
+
+		 ;; line and column
+		 "(" ;; '%02' to set to 2 chars at least; prevents flickering
+		 (propertize "%02l" 'face 'font-lock-type-face) ","
+		 (propertize "%02c" 'face 'font-lock-type-face)
+		 ") "
+
+		 ;; relative position, size of file
+		 "["
+		 (propertize "%p" 'face 'font-lock-constant-face) ;; % above top
+		 "/"
+		 (propertize "%I" 'face 'font-lock-constant-face) ;; size
+		 "] "
+
+		 ;; the current major mode for the buffer.
+		 "["
+
+		 '(:eval (propertize (codefalling//simplify-major-mode-name) 'face 'font-lock-string-face
+				     'help-echo buffer-file-coding-system))
+		 "] "
+
+
+		 "[" ;; insert vs overwrite mode, input-method in a tooltip
+		 '(:eval (propertize (if overwrite-mode "Ovr" "Ins")
+				     'face 'font-lock-preprocessor-face
+				     'help-echo (concat "Buffer is in "
+							(if overwrite-mode "overwrite" "insert") " mode")))
+
+		 ;; was this buffer modified since the last save?
+		 '(:eval (when (buffer-modified-p)
+			   (concat ","  (propertize "Mod"
+						    'face 'font-lock-warning-face
+						    'help-echo "Buffer has been modified"))))
+
+		 ;; is this buffer read-only?
+		 '(:eval (when buffer-read-only
+			   (concat ","  (propertize "RO"
+						    'face 'font-lock-type-face
+						    'help-echo "Buffer is read-only"))))
+		 "] "
+
+		 ;; evil state
+		 '(:eval (evil-generate-mode-line-tag evil-state))
+
+		 " "
+		 ;; add the time, with the date and the emacs uptime in the tooltip
+		 '(:eval (propertize (format-time-string "%H:%M")
+				     'help-echo
+				     (concat (format-time-string "%c; ")
+					     (emacs-uptime "Uptime:%hh")))))))
+
 (if (not *mode-line*)
     (add-hook 'after-init-hook #'(lambda () (setq-default mode-line-format nil)))
   (cond
@@ -61,65 +120,8 @@
       (doom-modeline-mode 1)))
    
    ((eq *mode-line* 'default)
-    (add-hook 'after-init-hook
-	      #'(lambda ()
-		  (interactive)
-		  (setq-default mode-line-format
-				(list
-				 ;; the buffer name; the file name as a tool tip
-				 " "
-				 '(:eval (propertize "%b " 'face 'font-lock-keyword-face
-						     'help-echo (buffer-file-name)))
-
-				 ;; line and column
-				 "(" ;; '%02' to set to 2 chars at least; prevents flickering
-				 (propertize "%02l" 'face 'font-lock-type-face) ","
-				 (propertize "%02c" 'face 'font-lock-type-face)
-				 ") "
-
-				 ;; relative position, size of file
-				 "["
-				 (propertize "%p" 'face 'font-lock-constant-face) ;; % above top
-				 "/"
-				 (propertize "%I" 'face 'font-lock-constant-face) ;; size
-				 "] "
-
-				 ;; the current major mode for the buffer.
-				 "["
-
-				 '(:eval (propertize (codefalling//simplify-major-mode-name) 'face 'font-lock-string-face
-						     'help-echo buffer-file-coding-system))
-				 "] "
-
-
-				 "[" ;; insert vs overwrite mode, input-method in a tooltip
-				 '(:eval (propertize (if overwrite-mode "Ovr" "Ins")
-						     'face 'font-lock-preprocessor-face
-						     'help-echo (concat "Buffer is in "
-									(if overwrite-mode "overwrite" "insert") " mode")))
-
-				 ;; was this buffer modified since the last save?
-				 '(:eval (when (buffer-modified-p)
-					   (concat ","  (propertize "Mod"
-								    'face 'font-lock-warning-face
-								    'help-echo "Buffer has been modified"))))
-
-				 ;; is this buffer read-only?
-				 '(:eval (when buffer-read-only
-					   (concat ","  (propertize "RO"
-								    'face 'font-lock-type-face
-								    'help-echo "Buffer is read-only"))))
-				 "] "
-
-				 ;; evil state
-				 '(:eval (evil-generate-mode-line-tag evil-state))
-
-				 " "
-				 ;; add the time, with the date and the emacs uptime in the tooltip
-				 '(:eval (propertize (format-time-string "%H:%M")
-						     'help-echo
-						     (concat (format-time-string "%c; ")
-							     (emacs-uptime "Uptime:%hh")))))))))
+    (add-hook 'after-init-hook 'pretty-default-modeline))
+   
    (t
     ())))
 
